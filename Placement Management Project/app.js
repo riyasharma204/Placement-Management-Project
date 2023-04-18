6ddbefcf990b771823fdfe11195267b21e5cfa6a
riyasharma204 <107679008+riyasharma204@users.noreply.github.com>
2023-04-18 19:45:07 +0530

    Add files via upload

---
+++ Placement Management Project/app.js
@@ -0,0 +1,46 @@
+//For Truviz
+$(".card").on("click", function(){
+    $(".detail").addClass("active");
+});
+
+$(".close-detail").on("click",function(){
+    $(".detail").removeClass("active")
+})
+
+//For Accenture
+$(".card2").on("click", function(){
+    $(".detail2").addClass("active");
+});
+
+$(".close-detail2").on("click",function(){
+    $(".detail2").removeClass("active")
+})
+
+//For Accenture
+$(".card3").on("click", function(){
+    $(".detail3").addClass("active");
+});
+
+$(".close-detail3").on("click",function(){
+    $(".detail3").removeClass("active")
+})
+
+//For Genpact
+$(".card4").on("click", function(){
+    $(".detail4").addClass("active");
+});
+
+$(".close-detail4").on("click",function(){
+    $(".detail4").removeClass("active")
+})
+
+//For Intel
+$(".card5").on("click", function(){
+    $(".detail5").addClass("active");
+});
+
+$(".close-detail5").on("click",function(){
+    $(".detail5").removeClass("active")
+})
+
+ 
